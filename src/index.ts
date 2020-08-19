9ec03df5ac9eb7f2717148470d76dac3e040fd5a
hsing <hsingyuc.7@gmail.com>
2020-08-20 00:43:51 +0800

    Remove sample input users.

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ import * as express from "express";
 import * as bodyParser from "body-parser";
 import { Request, Response } from "express";
 import { Routes } from "./routes";
-import { User } from "./entity/User";
 
 createConnection().then(async connection => {
 
